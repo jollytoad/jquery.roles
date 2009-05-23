eab0dbf4d1c376ce41c8c47845270c4719237a45
Mark Gibson <jollytoad@gmail.com>
2009-05-23 11:53:48 +0100

    Added general ARIA -> UI style support

---
+++ src/roles.aria.ui.js
@@ -0,0 +1,30 @@
+/*
+ * jQuery Roles - ARIA to jQuery UI styling @VERSION (@DATE)
+ *
+ * Copyright (c) 2009 Adaptavist.com
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ */
+/* Apply jQuery UI CSS Framework
+ *
+ * Depends:
+ *   roles.core.js
+ *   datatypes.core.js
+ */
+(function($) {
+
+$(':role')
+	.roleStage('style', function() {
+		$(this)
+			.bind('attr.@aria-hidden', function(event) {
+				$(this).toggleClass('ui-helper-hidden', $.dt.bool(event.newValue));
+			});
+	})
+	
+	.roleStage('init', function() {
+		$(this)
+			.initMutation('attr', 'aria-hidden', false);
+	});
+
+})(jQuery);
+
